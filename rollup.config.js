9667fd21cd27b64a62623c0e408d17943eb67a38
coderljw <coderljw@dingtalk.com>
2023-07-26 00:42:13 +0800

    build: 优化打包

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+import alias from '@rollup/plugin-alias'
+import json from '@rollup/plugin-json'
+import typescript from '@rollup/plugin-typescript'
+import merge from 'deepmerge'
+import { dirname } from 'path'
+import path from 'path'
+import { defineConfig } from 'rollup'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const baseConfig = defineConfig({
+  input: 'src/index.ts',
+  output: {
+    exports: 'named',
+    preserveModules: true,
+    banner: '#! /usr/bin/env node',
+  },
+  plugins: [
+    json(),
+    alias({
+      entries: [{ find: '@', replacement: path.join(__dirname, 'src') }],
+    }),
+  ],
+})
+
+export default defineConfig([
+  merge(baseConfig, {
+    output: { dir: 'dist/es', format: 'es' },
+    plugins: [typescript({ outDir: 'dist/es' })],
+  }),
+  merge(baseConfig, {
+    output: { dir: 'dist/lib', format: 'cjs' },
+    plugins: [typescript({ outDir: 'dist/lib' })],
+  }),
+])
